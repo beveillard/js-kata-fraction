bc3993ed35de6835afe38c905963b003c5c7a60d
Benoît Eveillard <benoit.eveillard@gmail.com>
2020-12-28 00:14:51 -0500

    feature(fraction) implement

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,29 @@
-import { systemUnderTest } from ".";
+import { sum, numerator, denominator } from ".";
 
-describe("Test of systemUnderTest()", function() {
-  it("Should pass", function() {
-    expect(systemUnderTest()).toEqual(true);
+describe("Test of numerator()", function () {
+
+  it("Should return 1", function () {
+    expect(numerator("1/2")).toEqual(1);
   });
+
+});
+
+describe("Test of denominator()", function () {
+
+  it("Should return 2", function () {
+    expect(denominator("1/2")).toEqual(2);
+  });
+
+});
+
+describe("Test of sum()", function () {
+
+  it("Should return 11/15", function () {
+    expect(sum("1/3", "2/5")).toEqual("11/15");
+  });
+
+  it("Should return 1/1", function () {
+    expect(sum("1/2", "1/2")).toEqual("1/1");
+  });
+
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,29 @@
-export function systemUnderTest(): boolean {
-  return true;
+export function sum(fraction1, fraction2) {
+  let n1 = numerator(fraction1);
+  let n2 = numerator(fraction2);
+  let d1 = denominator(fraction1);
+  let d2 = denominator(fraction2);
+
+  let sumNumerator = n1 * d2 + n2 * d1;
+  let sumDenominator = d1 * d2;
+
+  let sumGcd = gcd(sumNumerator, sumDenominator);
+
+  return String(sumNumerator / sumGcd) + "/" + String(sumDenominator / sumGcd);
+}
+
+export function numerator(fraction) {
+  let separator = fraction.indexOf('/');
+
+  return Number(fraction.slice(0, separator));
+}
+
+export function denominator(fraction) {
+  let separator = fraction.indexOf('/');
+
+  return Number(fraction.slice(separator + 1));
+}
+
+function gcd(a, b) {
+  return b === 0 ? a : gcd(b, a % b);
 }
