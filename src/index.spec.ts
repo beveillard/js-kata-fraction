be0f746a71a198e271f7b5e41ad50ef955fe4b09
Benoît Eveillard <benoit.eveillard@gmail.com>
2021-09-18 09:04:23 -0400

    Fraction

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,25 @@
-import { systemUnderTest } from ".";
+import { fraction, sum } from ".";
 
-describe("Test of systemUnderTest()", function() {
-  it("Should pass", function() {
-    expect(systemUnderTest()).toEqual(true);
+describe("Test of sum()", function () {
+
+  it("Should return 11/15", function () {
+    // Given
+    const f1 = { numerator: 1, denominator: 3 };
+    const f2 = { numerator: 2, denominator: 5 };
+    // When
+    const s = sum(f1, f2);
+    // Then
+    expect(s).toEqual({ numerator: 11, denominator: 15 });
   });
+
+  it("Should return 1/1", function () {
+    // Given
+    const f1 = { numerator: 1, denominator: 2 };
+    const f2 = { numerator: 1, denominator: 2 };
+    // When
+    const s = sum(f1, f2);
+    // Then
+    expect(s).toEqual({ numerator: 1, denominator: 1 });
+  });
+
 });
