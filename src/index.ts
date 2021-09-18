4dcd36e7695aa0e3d26e57dc3d425046fe59a087
Benoît Eveillard <benoit.eveillard@gmail.com>
2021-09-17 22:36:24 -0400

    Fraction

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-export function systemUnderTest(): boolean {
-  return true;
+export class fraction {
+  numerator: number;
+  denominator: number;
+}
+
+export function sum(f1: fraction, f2: fraction): fraction {
+  const sumNumerator = f1.numerator * f2.denominator + f2.numerator * f1.denominator;
+  const sumDenominator = f1.denominator * f2.denominator;
+  const sumGcd = gcd(sumNumerator, sumDenominator);
+
+  return {
+    numerator: sumNumerator / sumGcd,
+    denominator: sumDenominator / sumGcd,
+  };
+}
+
+function gcd(a: number, b: number): number {
+  return b === 0 ? a : gcd(b, a % b);
 }
